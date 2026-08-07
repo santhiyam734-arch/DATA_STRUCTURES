b4dbc951aa35f071e226fae42dd99c45eb9c3e5d
Santhiya M <santhiyam734@gmail.com>
2026-08-07 22:15:39 +0530

    Add files via upload

---
+++ Program-20-Equivalent Postfix/day20.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+
+#define MAX 100
+
+int isOperand(char ch)
+{
+    if((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z'))
+        return 1;
+    else
+        return 0;
+}
+
+int isOperator(char ch)
+{
+    if(ch=='+' || ch=='-' || ch=='*' || ch=='/' || ch=='^')
+        return 1;
+    else
+        return 0;
+}
+
+void infixToPostfix(char exp[])
+{
+    char stack[MAX];
+    int top = 0;
+    int i = 0;
+
+    printf("\nPostfix Expression : ");
+
+    while(exp[i] != '\0')
+    {
+        if(isOperand(exp[i]))
+        {
+            printf("%c", exp[i]);
+        }
+
+        else if(exp[i] == '(')
+        {
+            stack[top] = exp[i];
+            top++;
+        }
+
+        else if(isOperator(exp[i]))
+        {
+            stack[top] = exp[i];
+            top++;
+        }
+
+        else if(exp[i] == ')')
+        {
+            while(top > 0 && stack[top-1] != '(')
+            {
+                top--;
+                printf("%c", stack[top]);
+            }
+
+            if(top > 0)
+                top--;
+        }
+
+        i++;
+    }
+
+    while(top > 0)
+    {
+        top--;
+
+        if(stack[top] != '(')
+            printf("%c", stack[top]);
+    }
+
+    printf("\n");
+}
+
+int main()
+{
+    char exp[MAX];
+
+    printf("\nEnter Fully Parenthesized Expression: ");
+    scanf("%s", exp);
+
+    infixToPostfix(exp);
+
+    return 0;
+}
